2420991df6a23619012bb6ef248a255c18a9f039
zhanghaiyan <haiyan901@qq.com>
2016-04-01 14:13:16 +0800

    添加练习demo

---
+++ demos/demo1-let/let.js
@@ -0,0 +1,13 @@
+var a = [],b=[];
+for (let i = 0; i < 10; i++) {
+	a[i] = function () {
+		console.log(`array a use keyword "let",and 6th element\'s result is ${i}`);
+	};
+}
+for (var i = 0; i < 10; i++) {
+	b[i] = function () {
+		console.log(`array a use keyword "var",and 6th element\'s result is ${i}`);
+	};
+}
+a[5]();
+b[5]();
