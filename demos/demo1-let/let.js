b73b7b781d78e056fe4ae032f39e4cfbb18c4ced
zhanghaiyan <haiyan901@qq.com>
2016-04-01 14:19:03 +0800

    let和字符串模版的用法

--- demos/demo1-let/let.js
+++ demos/demo1-let/let.js
@@ -1,4 +1,5 @@
-var a = [],b=[];
+var a = [],b = [];
+//字符串模版引号是反撇号
 for (let i = 0; i < 10; i++) {
 	a[i] = function () {
 		console.log(`array a use keyword "let",and 6th element\'s result is ${i}`);
